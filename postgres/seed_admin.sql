056718eb152c3ca923956e7d67c0ff995c58c86d
n23borra <nathan.borrallo@imt-atlantique.net>
2026-03-26 13:39:13 +0100

    modif de l'adresse email avec celle du fablab

--- postgres/seed_admin.sql
+++ postgres/seed_admin.sql
@@ -1,13 +1,14 @@
 -- Seed script for creating an administrator account
 -- Generate a bcrypt hash for the password and replace <bcrypt-hash> below.
 -- Example (Unix): htpasswd -nbBC 10 "" strongpassword | tr -d ':\n'
+-- Actuellement le mot de passe est "strongpassword" pour les trois comptes, mais il est recommandé de le changer pour des raisons de sécurité.
 
 -- fichier: postgres/seed_admin.sql
 INSERT INTO users (username, email, password_hash, role)
 VALUES ('admin', 'admin@cyber.com', '$2y$10$6crrFQ2Jrl3Tekr7PdcJv.7M3ay8cns29J7UpG06kgRt.ldgdz9JW', 'ADMIN');
 INSERT INTO users (username, email, password_hash, role)
-VALUES ('superadmin', 'superadmin@cyber.com', '$2y$10$6crrFQ2Jrl3Tekr7PdcJv.7M3ay8cns29J7UpG06kgRt.ldgdz9JW',
+VALUES ('superadmin', 'superadmin@fablab.com', '$2y$10$6crrFQ2Jrl3Tekr7PdcJv.7M3ay8cns29J7UpG06kgRt.ldgdz9JW',
         'SUPER_ADMIN');
 INSERT INTO users (username, email, password_hash, role)
-VALUES ('Nathan', 'nathan.bidouille@gmail.com', '$2y$10$6crrFQ2Jrl3Tekr7PdcJv.7M3ay8cns29J7UpG06kgRt.ldgdz9JW',
+VALUES ('user', 'fablab.imta@gmail.com', '$2y$10$6crrFQ2Jrl3Tekr7PdcJv.7M3ay8cns29J7UpG06kgRt.ldgdz9JW',
         'USER');
